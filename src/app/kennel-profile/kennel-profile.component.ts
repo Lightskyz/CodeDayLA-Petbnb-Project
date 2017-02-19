8faf36e27c828f303d448f7aff22d5403ea9f52f
landaverde15 <landaverde.erik10@gmail.com>
2017-02-19 10:05:26 -0800

    input for place

--- src/app/kennel-profile/kennel-profile.component.ts
+++ src/app/kennel-profile/kennel-profile.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: 'app-kennel-profile',
-  templateUrl: './kennel-profile.component.html',
+
+  template: ` 
+  			  		<input type="text" #address placeholder="Address"/>
+  			  		<input type="text" #city placeholder="City"/>
+  			  		<input type="text" #state placeholder="State"/>
+  			  		<input type="text" #zipcode placeholder="Zipcode"/>
+  			  		<input type="text" #price placeholder="Price"/>
+  			   		<button class="button"(click)="addItem(address.value, city.value, state.value, zipcode.value, price.value )" routerLink="/pet-lover-component">Add</button>
+  			  
+  			`,
   styleUrls: ['./kennel-profile.component.css', '../../assets/css/bootstrap.min.css', '../../assets/css/material-kit.css', '../../assets/css/demo.css']
 })
-export class KennelProfileComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+export class KennelProfileComponent{
+	place: FirebaseListObservable<any []>
+  constructor(af: AngularFire) { 
+  	this.place = af.database.list('/place');
   }
+  
+  addItem(newAddress: string, newCity: string, newState: string, newZipcode: string, newPrice: string) {
+  	this.place.push({address: newAddress, city: newCity, state: newState, zipcode: newZipcode, price: newPrice})
+  	;
+  }	 
 
 }
